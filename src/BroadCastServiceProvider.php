fd3adae2ecabab45ab3bc5ec8d2d0adbac7f6d1c
soso <28512@yahoo.com.tw>
2016-08-03 19:15:38 +0800

    server side broadcast

---
+++ src/BroadCastServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BroadCastServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/Events' => base_path('app/Events'),
+            __DIR__.'/js' => base_path('resources/assets/js'),
+            __DIR__.'/gulpfile.js' => base_path(''),
+            __DIR__.'/socket.js' => base_path(''),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by User on 2016/8/3.
+ */
+var io = require('socket.io-client');
+
+// 建立 socket.io 的連線
+var notification = io.connect('http://localhost:3000');
+
+// 當從 socket.io server 收到 notification 時將訊息印在 console 上
+notification.on('notification', function(message) {
+    console.log(message);
+    document.getElementById("testtext").innerText = message;
+});
