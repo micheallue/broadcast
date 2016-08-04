9ce52310d7190dc0f55dfe58e74f34d379d86826
soso <28512@yahoo.com.tw>
2016-08-04 14:50:53 +0800

    add reconnect

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,36 @@
 var io = require('socket.io-client');
 
 // 建立 socket.io 的連線
-var notification = io.connect('http://localhost:3000');
+var notification = io.connect('http://192.168.1.106:3000');
 
 // 當從 socket.io server 收到 notification 時將訊息印在 console 上
+notification.on('disconnect', function() {
+    console.log('Got disconnect!');
+    notification.reconnect();
+});
+
+notification.on('reconnect_failed', function () {
+    try {
+        console.log('reconnect_failed');
+    } catch (e) {
+    }
+});
+
+notification.on('reconnect', function () {
+    try {
+        console.log('reconnect');
+        notification.reconnect();
+    } catch (e) {
+    }
+});
+
+notification.on('reconnecting', function () {
+    try {
+        console.log('reconnecting');
+    } catch (e) {
+    }
+});
+
 notification.on('notification', function(message) {
     console.log(message);
     //document.getElementById("testtext").innerText = message;
